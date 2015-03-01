03f4346295678e3b2696bbc73c91a4744b8cb13d
Nate Olson <nathandavidolson@gmail.com>
2015-03-01 00:31:21 -0500

    fixed split

--- get_fastq_pipeline.py
+++ get_fastq_pipeline.py
@@ -6,7 +6,7 @@ from prepc.sra_commands import *
 
 def main(analysis_params):
 	for i in ['miseq','pgm']:
-	    for j in analysis_params['accessions']:
+	    for j in analysis_params[i]['accessions']:
     		acc_log_dir = analysis_params['fastq_dir'] + "/log/" + j
         	subprocess.call(['mkdir','-p', acc_log_dir])
         	sra_get_fastq(i,j,analysis_params['fastq_dir'], acc_log_dir)
