958af14d0b6a5daaa911bf3577aff3eaa4d89e6a
Nate Olson <nathandavidolson@gmail.com>
2015-03-01 00:15:14 -0500

    fixed typo

--- get_fastq_pipeline.py
+++ get_fastq_pipeline.py
@@ -6,7 +6,7 @@ from prepc.sra_commands import *
 
 def main(analysis_params):
 	for i in ['miseq','pgm']:
-	    for j in analyis_params['accessions']:
-    		acc_log_dir = analyis_params['fastq_dir'] + "/log/" + j
+	    for j in analysis_params['accessions']:
+    		acc_log_dir = analysis_params['fastq_dir'] + "/log/" + j
         	subprocess.call(['mkdir','-p', acc_log_dir])
-        	sra_get_fastq(i,j,analyis_params['fastq_dir'], acc_log_dir)
+        	sra_get_fastq(i,j,analysis_params['fastq_dir'], acc_log_dir)
