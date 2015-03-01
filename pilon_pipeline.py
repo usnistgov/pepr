6e8ae8014fcdc5381d18a2e685631b77c6795177
Nate Olson <nathandavidolson@gmail.com>
2015-03-01 00:31:52 -0500

    fixed bam list

--- pilon_pipeline.py
+++ pilon_pipeline.py
@@ -6,19 +6,18 @@
 from prepc.samtools_commands import *
 from prepc.pilon_commands import *
 
-def write_bam_list_to_file(analysis_params, filename):
+def write_bam_list_to_file(analysis_params):
     ## write bam list to file
     bam_list = []
     for i in analysis_params['miseq']['accessions']:
         bam_list.append(analysis_params[i]['sorted_bam'])
-    bam_list_file = open(filename, 'w')
+    bam_list_file = open(analysis_params['miseq']['pilon_bam_list'], 'w')
     bam_list_file.write("\n".join(bam_list))
     bam_list_file.close()
 
 def main(analysis_params):
     print "Running pilon pipeline ..."
-    write_bam_list_to_file(bam_list = analysis_params['miseq']['accessions'], \
-                           filename=analysis_params['miseq']['pilon_bam_list'])
+    write_bam_list_to_file(analysis_params)
     
     samtools_bam_merge(bam_list=analysis_params['miseq']['pilon_bam_list'], \
                        out_bam=analysis_params['miseq']['pilon_merged_bam'],\
