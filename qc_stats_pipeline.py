58e39ae54fca475a912e5d2af40cb51286f5659c
Nate Olson <nathandavidolson@gmail.com>
2015-03-05 16:09:42 -0500

    modifed for yaml arguments

--- qc_stats_pipeline.py
+++ qc_stats_pipeline.py
@@ -2,7 +2,6 @@
 # 
 # Input: fastq, and mapped bam files    
 # Output: summary stat files from picard and fastqc
-
 import sys
 import re
 import subprocess
@@ -10,35 +9,31 @@ from prepc.parse_pipeline_params import *
 from prepc.qc_stats_commands import *
 from prepc.picard_commands import *
 
-def run_qc_stat_pipeline(run_params,pipeline_params):
+def main(analysis_params):
+    for i in analysis_params['accessions']:
+        ## processing fastq
+        fastqc_stats(in_fq = analysis_params[i]['fastq1'],
+                     out_dir = run_params['qc_stats']['analysis_dir'], 
+                     log_dir = analysis_params['qc_stats']['log_dir'])
+        if run_params['fastq2']:
+            fastqc_stats(in_fq = analysis_params[i]['fastq1'],
+                     out_dir = analysis_params['qc_stats']['analysis_dir'], 
+                     log_dir = analysis_params['qc_stats']['log_dir'])       
+
+        ## processing bam
+        picard_multiple_metrics(in_bam = analysis_params[i]['sorted_bam'],
+                                 bam_stats = analysis_params[i]['bam_metrics'], 
+                                 log_dir = ranalysis_params['qc_stats']['log_dir'])
+
+
+# def main(filename):
+#     #read run parameters from input file and map reads to reference using bwa
+#     parameters = read_params(filename)
     
-    ## processing fastq
-    fastqc_stats(in_fq = run_params['fastq1'],
-                 out_dir = run_params['out_dir'], 
-                 log_dir = run_params['log_dir'])
-    if run_params['fastq2']:
-        fastqc_stats(in_fq = run_params['fastq2'],
-                     out_dir = run_params['out_dir'], 
-                     log_dir = run_params['log_dir'])       
+#     # creating temp directory
+#     subprocess.call(['mkdir','-p',parameters['root_dir']+ parameters['analysis_out_dir']+"/tmp/"])
 
-    ## processing bam
-    picard_multiple_metrics(in_bam = run_params['sorted_bam'],
-                             bam_stats = run_params['bam_metrics'], 
-                             log_dir = run_params['log_dir'])
-
-
-def main(filename):
-    #read run parameters from input file and map reads to reference using bwa
-    parameters = read_params(filename)
-    
-    # creating temp directory
-    subprocess.call(['mkdir','-p',parameters['root_dir']+ parameters['analysis_out_dir']+"/tmp/"])
-
-    for i in parameters['datasets'].split(","):
-        run_params = define_mapping_params(i,parameters)
-        record_params(run_params,parameters)
-        run_qc_stat_pipeline(run_params, parameters)
-
-# if __name__ == '__main__':
-#     main(sys.argv[1])
-
+#     for i in parameters['datasets'].split(","):
+#         run_params = define_mapping_params(i,parameters)
+#         record_params(run_params,parameters)
+#         run_qc_stat_pipeline(run_params, parameters)
