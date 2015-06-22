1cae3c3ca2d9a97c6ea814e5fd67afdfc93fa458
Nate Olson <nathandavidolson@gmail.com>
2015-06-22 07:46:33 -0400

    modified java heap size

--- prepc/gatk_commands.py
+++ prepc/gatk_commands.py
@@ -11,7 +11,7 @@ def gatk_realign(in_ref, in_bam, out_bam, intervals_file, log_dir):
     stderr_file = open(log_dir + "/gatk_realign"+ time.strftime("-%Y-%m-%d-%H-%M-%S.stder"),'w')
     
     # run commands
-    GATK_command = ["java","-jar","-Xmx16g","/pepr/utils/GenomeAnalysisTK.jar"]
+    GATK_command = ["java","-jar","-Xmx8g","/pepr/utils/GenomeAnalysisTK.jar"]
     realigner_target_command = GATK_command + ["-T","RealignerTargetCreator", "-R",in_ref,"-I",in_bam, "-o", intervals_file]
     subprocess.call(realigner_target_command,stdout=log_file,stderr=stderr_file)
     
