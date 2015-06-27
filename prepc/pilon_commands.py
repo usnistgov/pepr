4431c2d528609332a24904e476058ec4b6cc9fd7
nate-d-olson <nolson@nist.gov>
2015-06-27 12:26:29 -0400

    Update pilon_commands.py
    increased ram for ec2 run

--- prepc/pilon_commands.py
+++ prepc/pilon_commands.py
@@ -12,7 +12,7 @@ def pilon_fixassembly(in_ref, in_bam, out_root, log_dir, input_type):
     stderr_file = open(log_dir + "/pilon-fixassembly"+ time.strftime("-%Y-%m-%d-%H-%M-%S.stder"),'w')
     
     # run command
-    pilon_command = ["java","-Xmx16G","-jar","/usr/local/bin/pilon.jar",
+    pilon_command = ["java","-Xmx24G","-jar","/usr/local/bin/pilon.jar",
                     "--genome",in_ref,input_type,in_bam,"--changes","--vcf","--tracks",
                     "--fix","all,breaks,novel","--output", out_root]
     subprocess.call(pilon_command, stdout=log_file,stderr=stderr_file) 
