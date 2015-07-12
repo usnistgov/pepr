0eda41d774cc7b8db05dd71ec694f4dcd0a0f186
nate-d-olson <nolson@nist.gov>
2015-07-11 22:08:19 -0400

    fixed file check bug

--- map_pipeline.py
+++ map_pipeline.py
@@ -4,13 +4,14 @@ from prepc.bwa_commands import *
 from prepc.sam_to_bam_pipeline import *
 from prepc.refine_bam_pipeline import main as refine_bam_pipeline
 import warnings
+import os
 
 def main(analysis_params, refine = False):
 	for i in analysis_params['accessions']:
 		if analysis_params[i]['plat'] not in ['pgm','miseq', 'pacbio']:
 			message = "Accession %s not run, only accessions with plat values 'pgm','miseq'. 'pacbio' are run" % (i)
 			warnings.warn(message)
-		elif not os.path.isfile(analysis_params[accession]['sorted_bam']) :
+		elif os.path.isfile(analysis_params[accession]['sorted_bam']) :
 			print "Raw bam present skip mapping"
 		else:
 			print "Mapping %s" % i
@@ -35,11 +36,11 @@ def main(analysis_params, refine = False):
 	        sam_to_bam(i, analysis_params[i])
 
 	        #fix pairs, markdup, realignment around indels
-        if refine:
-	        if os.path.isfile(analysis_params[accession]['markdup_file']):
-	        	print "Refine bam present skip refine mapping"
-	        elif analysis_params[i]['plat'] == 'pacbio':
-   					analysis_params[i]['markdup_file'] = accession_params[i]['sorted_bam']
-	    	else:
-		    	refine_bam_pipeline(i, analysis_params['ref'], analysis_params[i])
+        	if refine:
+	        	if os.path.isfile(analysis_params[accession]['markdup_file']):
+	        		print "Refine bam present skip refine mapping"
+			elif analysis_params[i]['plat'] == 'pacbio':
+   				analysis_params[i]['markdup_file'] = accession_params[i]['sorted_bam']
+	    		else:
+		    		refine_bam_pipeline(i, analysis_params['ref'], analysis_params[i])
 
