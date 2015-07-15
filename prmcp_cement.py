780b83ad89d22c3cb4be7a9cfce10f8c8d40284a
nate-d-olson <nolson@nist.gov>
2015-07-15 08:23:39 -0400

    cleanup2

---
+++ prmcp_cement.py
@@ -0,0 +1,86 @@
+from cement.core import backend, foundation, controller, handler
+from prmcp_pipeline import *
+
+# define an application base controller
+class MyAppBaseController(controller.CementBaseController):
+    class Meta:
+        label = 'base'
+        description = "My Application does amazing things!"
+
+        # config_defaults = dict(
+        #     foo='bar',
+        #     some_other_option='my default value',
+        #     )
+
+        # arguments = [
+        #     (['-L', '--Lpipeline'], dict(dest='blank', action='store', help='defining pipeline to run')),
+        #     (['-C'], dict(action='store_true', help='the big C option'))
+        #     ]
+        # need to workout defining pipeline to run ...
+    @controller.expose(hide=True, aliases=['run'])
+    def default(self):
+        if self.app.pargs.config:
+            self.app.log.info("Processing config file '%s'." % \
+                         self.app.pargs.config)
+            if self.app.pargs.pipe:
+                self.app.log.info("Running '%s' pipeline." % \
+                         self.app.pargs.pipe)
+                if "%s" % self.app.pargs.pipe == "evaluation":
+                    run_genome_eval_pipeline(self.app.pargs.config)
+                elif "%s" % self.app.pargs.pipe == "re-evaluation":
+                    run_genome_eval_pipeline(self.app.pargs.config, pipe = "skip_get_fastq")
+                elif "%s" % self.app.pargs.pipe == "characterization":
+                    run_genome_characterization_pipeline(self.app.pargs.config)
+                else:
+                    self.app.log.info("Define which pipeline to run using -P")
+            else:
+                self.app.log.info("Define which pipeline to run using -P")     
+        else:
+            self.app.log.info("Pipeline requires config file identified with -c")
+            
+"""
+    @controller.expose(help="this command does relatively nothing useful.")
+    def command1(self):
+        self.app.log.info("Inside base.command1 function.")
+
+    @controller.expose(aliases=['cmd2'], help="more of nothing.")
+    def command2(self):
+        self.app.log.info("Inside base.command2 function.")
+
+# define a second controller
+class MySecondController(controller.CementBaseController):
+    class Meta:
+        label = 'secondary'
+        stacked_on = 'base'
+
+    @controller.expose(help='this is some command', aliases=['some-cmd'])
+    def some_other_command(self):
+        pass
+"""
+
+class MyApp(foundation.CementApp):
+    class Meta:
+        label = 'mainpipe'
+        base_controller = MyAppBaseController
+
+# create the app
+app = MyApp()
+
+# Register any handlers that aren't passed directly to CementApp
+# handler.register(MySecondController)
+
+try:
+    # setup the application
+    app.setup()
+
+    # add arguments to the parser
+    app.args.add_argument('-c', '--config', action='store', metavar='STR',
+                          help='yaml pipeline parameters file')
+    app.args.add_argument('-P', '--pipe', action='store', metavar='STR',
+                          help='define which pipeline to run')
+
+    # run the application
+    app.run()
+finally:
+    # close the app
+    app.close()
---
+++ prmcp_pipeline.py
@@ -0,0 +1,152 @@
+import yaml
+from get_fastq_pipeline import main as get_fastq_pipeline 
+from index_ref_pipeline import main as index_ref_pipeline 
+from map_pipeline import main as map_pipeline 
+from pilon_pipeline import main as pilon_pipeline 
+from qc_stats_pipeline import main as qc_stats_pipeline 
+from homogeneity_analysis_pipeline import main as homogeneity_analysis_pipeline 
+from consensus_base_pipeline import main as consensus_base_pipeline 
+from prepc.define_pipeline_params import *
+
+def run_genome_eval_pipeline_base(parameters):
+	''' Full genome evaluation pipeline takes an input parameter file and;
+	1. retrieves fastq files from SRA archive for provided Genbank accessions
+	2. indexes user provided reference genome sequence
+	3. maps miseq reads to user provided reference genome 
+	4. evaluates reference genome using pilon_pipeline with miseq data
+	'''
+	print "defining run parameters"
+	param_file = file(parameters, 'r')
+	pipeline_params = yaml.load(param_file)
+
+	#initiating project
+	analysis_params = init_prj(pipeline_params)
+	init_params(pipeline_params, analysis_params)
+
+	#mapping params
+	init_analysis('mapping', analysis_params, run_by = 'accession')
+	for i in analysis_params['accessions']:
+		define_map_run(i, analysis_params, pipeline_params)
+
+	#pilon params
+	init_analysis('pilon', analysis_params, run_by = 'plat')
+	define_pilon_run('miseq', analysis_params)
+
+	print "printing pipeline parameters to file ..."
+	param_out = analysis_params['prj_dir'] + "/" + analysis_params['ref_root'] + \
+							"_" + pipeline_params['project_id'] + "_evaluation_parameters.yaml"
+	with open(param_out, 'w') as f:
+  		yaml.dump(analysis_params, f, default_flow_style=False, encoding = None)
+
+def run_genome_eval_pipeline(parameters, pipe = "full"):
+	''' Full genome evaluation pipeline takes an input parameter file and;
+	1. retrieves fastq files from SRA archive for provided Genbank accessions
+	2. indexes user provided reference genome sequence
+	3. maps miseq reads to user provided reference genome 
+	4. evaluates reference genome using pilon_pipeline with miseq data
+	'''
+	print "defining run parameters"
+	param_file = file(parameters, 'r')
+	pipeline_params = yaml.load(param_file)
+
+	#initiating project
+	analysis_params = init_prj(pipeline_params)
+	init_params(pipeline_params, analysis_params)
+
+	#mapping params
+	init_analysis('mapping', analysis_params, run_by = 'accession')
+	for i in analysis_params['accessions']:
+		define_map_run(i, analysis_params, pipeline_params)
+
+	#pilon params
+	init_analysis('pilon', analysis_params, run_by = 'plat')
+	define_pilon_run('miseq', analysis_params)
+
+	print "printing pipeline parameters to file ..."
+	param_out = analysis_params['prj_dir'] + "/" + analysis_params['ref_root'] + \
+							"_" + pipeline_params['project_id'] + "_" + \
+							pipe + "_evaluation_parameters.yaml"
+	with open(param_out, 'w') as f:
+  		yaml.dump(analysis_params, f, default_flow_style=False, encoding = None)
+
+	print "Running step 1 of 4"
+	if(pipe != 'full'):
+		print "skipping fastq download"
+	else:
+		print "downloading fastq"
+		get_fastq_pipeline(analysis_params)
+
+
+	print "Running step 2 of 4"
+	index_ref_pipeline(analysis_params)
+
+
+	print "Running step 3 of 4"
+	map_pipeline(analysis_params)
+
+	print "Running step 4 of 4"
+	pilon_pipeline(analysis_params)
+
+	
+
+def run_genome_characterization_pipeline(parameters):
+	''' Full prmcp miseq pipeline takes an input parameter file and;
+	1. indexes reference
+	2. maps all reads to reference
+	3. calculates fastq and bam summary statistics
+	4. performs homogeneity analysis (pairwise variant calling) for miseq data
+	5. generates base level summary for reads mapped to the genome
+	'''	
+	# not sure whether to include indexing and mapping reads to reference or just use output from re-eval?
+	print "defining run parameters"
+	param_file = file(parameters, 'r')
+	pipeline_params = yaml.load(param_file)
+
+	#initiating project
+	analysis_params = init_prj(pipeline_params)
+	init_params(pipeline_params, analysis_params)
+
+	#mapping params
+	init_analysis('mapping', analysis_params, run_by = 'accession')
+	for i in analysis_params['accessions']:
+		define_map_run(i, analysis_params, pipeline_params)
+
+	#qc_stats params
+	init_analysis('qc_stats', analysis_params, run_by = 'accession')
+	for i in analysis_params['accessions']:
+		define_qc_run(i, analysis_params)
+
+	#consensus_base params
+	init_analysis('consensus_base', analysis_params, run_by = 'plat')
+	for i in ['miseq','pgm']:
+		define_consensus_base_run(i, analysis_params)
+
+	# Still need to work out bug in parameter definitions
+	#homogeneity params
+	init_analysis('homogeneity', analysis_params, run_by = 'miseq_pairs')
+	for i in analysis_params['pairs']:
+		acc1, acc2 = i.split("-")
+		define_homogeneity_run(acc1, acc2, analysis_params)	
+
+	print "printing pipeline parameters to file ..."
+	param_out = analysis_params['prj_dir'] + "/" + analysis_params['ref_root'] + \
+							"_" + pipeline_params['project_id'] + "_charaterization_parameters.yaml"
+	with open(param_out, 'w') as f:
+  		yaml.dump(analysis_params, f, default_flow_style=False, encoding = None)
+
+
+	print "Running step 1 of 5"
+	index_ref_pipeline(analysis_params)
+
+	print "Running step 2 of 5"
+	map_pipeline(analysis_params, refine = True)
+
+
+	print "Running step 3 of 5"
+	qc_stats_pipeline(analysis_params)
+
+	print "Running step 4 of 5"
+	homogeneity_analysis_pipeline(analysis_params)
+
+	print "Running step 5 of 5"
+	consensus_base_pipeline(analysis_params)
