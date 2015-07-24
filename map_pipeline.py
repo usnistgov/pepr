8c10f86438669796e14f970cdfe900f16d704680
nate-d-olson <nolson@nist.gov>
2015-07-24 17:37:05 -0400

    added refine_bam function

--- map_pipeline.py
+++ map_pipeline.py
@@ -6,6 +6,15 @@ from prepc.refine_bam_pipeline import main as refine_bam_pipeline
 import warnings
 import os
 
+def refine_bam(analysis_params, accession):
+	if os.path.isfile(analysis_params[accession]['markdup_file']):
+	    print "Refine bam present skip refine mapping"
+	#elif analysis_params[accession['plat'] == 'pacbio':
+   	#	analysis_params[accession]['markdup_file'] = accession_params[accession]['sorted_bam']
+	else:
+		refine_bam_pipeline(accession, analysis_params['ref'], analysis_params[accession])
+
+
 def main(analysis_params, refine = False):
 	for i in analysis_params['accessions']:
 		print "preparing to map %s" % i
@@ -14,6 +23,8 @@ def main(analysis_params, refine = False):
 			warnings.warn(message)
 		elif os.path.isfile(analysis_params[i]['sorted_bam']) :
 			print "Raw bam present skip mapping"
+			if refine:
+				refine_bam(analysis_params, i)
 		else:
 			print "Mapping %s" % i
 			if analysis_params[i]['plat'] == 'pgm':
@@ -33,15 +44,10 @@ def main(analysis_params, refine = False):
 					in_fq2 = analysis_params[i]['fastq2'],
 					out_sam = analysis_params[i]['sam'],
 					log_dir = analysis_params[i]['mapping_log'])	
-	        #sorting, indexing and adding header
-	        sam_to_bam(i, analysis_params[i])
+			#sorting, indexing and adding header
+			sam_to_bam(i, analysis_params[i])
 
 	        #fix pairs, markdup, realignment around indels
-        if refine:
-	        if os.path.isfile(analysis_params[i]['markdup_file']):
-	        	print "Refine bam present skip refine mapping"
-	        elif analysis_params[i]['plat'] == 'pacbio':
-   					analysis_params[i]['markdup_file'] = accession_params[i]['sorted_bam']
-	    	else:
-		    	refine_bam_pipeline(i, analysis_params['ref'], analysis_params[i])
-
+        	if refine:
+        		refine_bam(analysis_params, i)
+	       
