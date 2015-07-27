5468ce8209dd582386a5bbded8199998056e52ed
nate-d-olson <nolson@nist.gov>
2015-07-27 18:23:08 -0400

    added parallelization to qc_stats_pipeline

--- qc_stats_pipeline.py
+++ qc_stats_pipeline.py
@@ -10,8 +10,34 @@ import os
 from prepc.picard_commands import picard_multiple_metrics
 from prepc.samtools_commands import samtools_depth
 
+from joblib import Parallel, delayed  
+import multiprocessing
+num_cores = multiprocessing.cpu_count()
+
+def parallelQCstats(i, analysis_params):
+    ## variable check
+    assert i
+    assert analysis_params
+    assert analysis_params[i]['bam_metrics']
+    assert analysis_params[i]['bam_depth']
+
+    ## input directory and file check
+    assert os.path.isfile(analysis_params[i]['sorted_bam'])
+    assert os.path.isdir( analysis_params['qc_stats']['log_dir'])
+
+    picard_multiple_metrics(in_bam = analysis_params[i]['sorted_bam'],
+                                 bam_stats = analysis_params[i]['bam_metrics'], 
+                                 log_dir = analysis_params['qc_stats']['log_dir'])
+
+    samtools_depth(in_bam = analysis_params[i]['sorted_bam'], 
+                    out_depth =analysis_params[i]['bam_depth'], 
+                    log_dir = analysis_params['qc_stats']['log_dir']) 
+
 def main(analysis_params):
-    for i in analysis_params['accessions']:
+    Parallel(n_jobs=num_cores)(delayed(parallelQCstats)(i, analysis_params) \
+            for i in analysis_params['accessions'])
+
+    # for i in analysis_params['accessions']:
         ## processing fastq
         # fastqc_stats(in_fq = analysis_params[i]['fastq1'],
         #              out_dir = analysis_params['qc_stats']['analysis_dir'], 
@@ -22,13 +48,13 @@ def main(analysis_params):
         #              log_dir = analysis_params['qc_stats']['log_dir'])       
 
         ## processing bam
-        picard_multiple_metrics(in_bam = analysis_params[i]['sorted_bam'],
-                                 bam_stats = analysis_params[i]['bam_metrics'], 
-                                 log_dir = analysis_params['qc_stats']['log_dir'])
+        # picard_multiple_metrics(in_bam = analysis_params[i]['sorted_bam'],
+        #                          bam_stats = analysis_params[i]['bam_metrics'], 
+        #                          log_dir = analysis_params['qc_stats']['log_dir'])
 
-        samtools_depth(in_bam = analysis_params[i]['sorted_bam'], 
-                        out_depth =analysis_params[i]['bam_depth'], 
-                        log_dir = analysis_params['qc_stats']['log_dir']) 
+        # samtools_depth(in_bam = analysis_params[i]['sorted_bam'], 
+        #                 out_depth =analysis_params[i]['bam_depth'], 
+        #                 log_dir = analysis_params['qc_stats']['log_dir']) 
 
 
 # def main(filename):
