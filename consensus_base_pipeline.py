ca31a90cccacad5bd3a035308496808f2a5c805e
nate-d-olson <nolson@nist.gov>
2015-08-01 11:07:45 -0400

    added parallelization

--- consensus_base_pipeline.py
+++ consensus_base_pipeline.py
@@ -1,18 +1,21 @@
 # ###Whole genome vcf files
-import sys
-import re
-import subprocess
 from prepc.samtools_commands import *
 from prepc.vcflib_commands import *
+from joblib import Parallel, delayed 
+import multiprocessing
+num_cores = multiprocessing.cpu_count()
 
-def main(analysis_params):
-    for i in analysis_params['plat']:
-        # whole genome variant calls
-        bam_list = [analysis_params[j]['markdup_file'] for j in analysis_params[i]['accessions']]
-        samtools_mpileup(in_ref=analysis_params['ref'],
-                         in_bams=bam_list,
-                         out_vcf=analysis_params[i]['consensus_vcf'],
-                         log_dir=analysis_params[i]['consensus_base_log'])
-        vcflib_vcf2tsv(in_vcf= analysis_params[i]['consensus_vcf'],
-        			   out_tsv= analysis_params[i]['consensus_tsv'],
-        			   log_dir=analysis_params[i]['consensus_base_log'])
+def parallelConsensus(plat, analysis_parmas):
+    # whole genome variant calls
+    bam_list = [analysis_params[i]['markdup_file'] for i in analysis_params[plat]['accessions']]
+    samtools_mpileup(in_ref=analysis_params['ref'],
+                     in_bams=bam_list,
+                     out_vcf=analysis_params[plat]['consensus_vcf'],
+                     log_dir=analysis_params[plat]['consensus_base_log'])
+    vcflib_vcf2tsv(in_vcf= analysis_params[plat]['consensus_vcf'],
+                   out_tsv= analysis_params[plat]['consensus_tsv'],
+                   log_dir=analysis_params[plat]['consensus_base_log'])   
+
+def main(analysis_params, platforms = analysis_params['plat']):
+    Parallel(n_jobs=num_cores)(delayed(parallelConsensus)(i, analysis_params) for i in platforms)
+
