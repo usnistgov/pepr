5eb6158b918ed53b108a7812686f2417aacea1a9
nate-d-olson <nolson@nist.gov>
2015-08-01 12:46:36 -0400

    adding parallelization for mpileup

--- homogeneity_analysis_pipeline.py
+++ homogeneity_analysis_pipeline.py
@@ -17,51 +17,31 @@ def parallelVarscan(i, analysis_params):
                         indel_out=analysis_params[i]['varscan_indel_file'], 
                         log_dir=analysis_params[i]['homogeneity_log'])
 
+def parallelVarscanMpileip(i, analysis_params):
+    assert analysis_params[i]['mpileup_file']
+    assert os.path.isfile(analysis_params[i]['markdup_file'])
+
+
+    if not os.path.isfile(analysis_params[i]['mpileup_file']) or \
+        os.path.getsize(analysis_params[i]['mpileup_file']) == 0:
+
+        samtools_mpileup_single(in_ref= analysis_params['ref'], 
+                                in_bam= analysis_params[i]['markdup_file'],
+                                out_mpileup=analysis_params[i]['mpileup_file'], 
+                                log_dir=analysis_params['homogeneity']['log_dir'])
+
+        assert os.path.isfile(analysis_params[i]['mpileup_file'])
+        assert os.path.getsize(analysis_params[i]['mpileup_file']) > 0
+    else:
+        print "skipping mpileup for %s" % i
+
 def main(analysis_params):
     print "Homogeneity analysis"
     assert os.path.isfile(analysis_params['ref'])
     assert os.path.isdir(analysis_params['homogeneity']['log_dir'])
 
-    for i in analysis_params['miseq']['accessions']:
-        assert analysis_params[i]['mpileup_file']
-        assert os.path.isfile(analysis_params[i]['markdup_file'])
+    # parallel mpileup
+    Parallel(n_jobs=num_cores)(delayed(parallelVarscanMpileup)(i, analysis_params) for i in analysis_params['miseq']['accessions'])
 
-
-        if not os.path.isfile(analysis_params[i]['mpileup_file']) or \
-            os.path.getsize(analysis_params[i]['mpileup_file']) == 0:
-
-            samtools_mpileup_single(in_ref= analysis_params['ref'], 
-                                    in_bam= analysis_params[i]['markdup_file'],
-                                    out_mpileup=analysis_params[i]['mpileup_file'], 
-                                    log_dir=analysis_params['homogeneity']['log_dir'])
-
-            assert os.path.isfile(analysis_params[i]['mpileup_file'])
-            assert os.path.getsize(analysis_params[i]['mpileup_file']) > 0
-        else:
-            print "skipping mpileup for %s" % i
-
-    #for i in analysis_params['pairs']:
+    # parallel varscan
     Parallel(n_jobs=num_cores)(delayed(parallelVarscan)(i, analysis_params) for i in analysis_params['pairs'])
-
-        # varscan_somatic(in_mpileup1=analysis_params[i]['mpileup_file1'], 
-        #                 in_mpileup2=analysis_params[i]['mpileup_file2'], 
-        #                 snp_out=analysis_params[i]['varscan_snp_file'], 
-        #                 indel_out=analysis_params[i]['varscan_indel_file'], 
-        #                 log_dir=analysis_params[i]['homogeneity_log'])            
-
-
-
-# if __name__ == '__main__':
-#     main(sys.argv[1])
-
-# Code from Justin used to characterize human RM
-'''
-echo "Running job $JOB_NAME, $JOB_ID on $HOSTNAME"
-   path/samtools-0.1.18/samtools mpileup -q 1 -f /projects/justin.zook/from-projects/references/human_g1k_v37.fasta $BAM1 $BAM2 | java -jar -Xmx2g path/varscan/VarScan.v2.3.6.jar somatic - --output-snp "$OUTSTART"_snp.txt --output-indel "$OUTSTART"_indel.txt --mpileup 1 --min-coverage $COV  --min-coverage-tumor $COV --min-coverage-normal $COV --somatic-p-value 0.001
-
-# hold off for now ....
-#Filter output for sites with cov < 300 and change to csv for analysis in R
-cat "$OUTSTART"_indel.txt | awk '$5 + $6 < 300' | awk '{ print $1,$2,$3,$4,$5,$6,$9,$10,$15 }' | sed 's/\ /,/g' > "$OUTSTART"_indel_covlt300.csv
-cat "$OUTSTART"_snp.txt | awk '$5 + $6 < 300' | awk '{ print $1,$2,$3,$4,$5,$6,$9,$10,$15 }' | sed 's/\ /,/g' > "$OUTSTART"_snp_covlt300.csv
-
-'''
